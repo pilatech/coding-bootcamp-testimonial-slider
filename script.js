f62e86bcfcf0971208e578240d8f4587715b37fb
pilate <pilatech6@gmail.com>
2022-08-08 17:50:19 +0200

    minor fix

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const testimonials = [
  {
-  name: 'John Tarkpor',
-  position: 'Junior Front-end Developer',
-  testimonial: `If you want to lay the best foundation possible I’d recommend taking this course. 
-  The depth the instructors go into is incredible. I now feel so confident about 
-  starting up as a professional developer.`,
-  image: 'images/image-john.jpg'
- },
- {
   name: 'Tany Sinclair',
   position: 'UX Engineer',
   testimonial: `I’ve been interested in coding for a while but never taken the jump, until now. 
   I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
   excited about the future.`,
   image: 'images/image-tanya.jpg'
+ },
+ {
+  name: 'John Tarkpor',
+  position: 'Junior Front-end Developer',
+  testimonial: `If you want to lay the best foundation possible I’d recommend taking this course. 
+  The depth the instructors go into is incredible. I now feel so confident about 
+  starting up as a professional developer.`,
+  image: 'images/image-john.jpg'
  }
 ]
 
