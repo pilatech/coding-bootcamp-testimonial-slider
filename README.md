628cc5f2d8ce93e07ba4f1a5bdc2620a14545cbf
pilate <pilatech6@gmail.com>
2022-08-08 17:44:21 +0200

    complete: added css, js

---
+++ README.md
@@ -0,0 +1,45 @@
+# Frontend Mentor - Coding bootcamp testimonials slider solution
+
+This is a solution to the [Coding bootcamp testimonials slider challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/coding-bootcamp-testimonials-slider-4FNyLA8JL). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+Testimonial Slider with HTML, CSS & JavaScript
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the component depending on their device's screen size
+- Navigate the slider using either their mouse/trackpad or keyboard
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- Solution URL: [here](https://github.com/pilatech/coding-bootcamp-testimonial-slider)
+- Live Site URL: [here](https://coding-bootcamp-testimonial-slider-by-pilate.netlify.app/)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+## Author
+
+- Frontend Mentor - [@pilatech](https://www.frontendmentor.io/profile/pilatech)
+- Twitter - [@pchinyengetere](https://www.twitter.com/pchinyengetere)
---
+++ script.js
@@ -0,0 +1,52 @@
+const testimonials = [
+ {
+  name: 'John Tarkpor',
+  position: 'Junior Front-end Developer',
+  testimonial: `If you want to lay the best foundation possible I’d recommend taking this course. 
+  The depth the instructors go into is incredible. I now feel so confident about 
+  starting up as a professional developer.`,
+  image: 'images/image-john.jpg'
+ },
+ {
+  name: 'Tany Sinclair',
+  position: 'UX Engineer',
+  testimonial: `I’ve been interested in coding for a while but never taken the jump, until now. 
+  I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
+  excited about the future.`,
+  image: 'images/image-tanya.jpg'
+ }
+]
+
+
+const btnNext = document.querySelector('.btn--next')
+const btnPrev = document.querySelector('.btn--prev')
+let index = 0
+let currentItem = testimonials[index]
+
+updateUI(currentItem)
+
+btnNext.addEventListener('click', e => {
+ index === testimonials.length - 1 ? index = 0 : index = index + 1
+
+ currentItem = testimonials[index]
+ updateUI(currentItem)
+})
+
+btnPrev.addEventListener('click', e => {
+ index === 0 ? index = testimonials.length - 1 : index = index - 1
+
+ currentItem = testimonials[index]
+ updateUI(currentItem)
+})
+
+function updateUI(currentItem){
+  console.log(currentItem)
+  const testimonial = document.querySelector('.testimonial__quote')
+  const name = document.querySelector('.testimonial__author')
+  const position = document.querySelector('.testimonial__position')
+  const hero = document.querySelector('.testimonial__hero')
+  testimonial.textContent = currentItem.testimonial
+  name.textContent = currentItem.name
+  position.textContent = currentItem.position
+  hero.style.backgroundImage = `url(${currentItem.image})`
+}
